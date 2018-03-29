d11ec674744a01fcd72b9bc6e2efdb6dc29e05f4
m.vanduijn <m.vanduijn@zefiros.eu>
2018-03-30 00:47:09 +0200

    Export links for linux

--- .export.lua
+++ .export.lua
@@ -15,6 +15,58 @@ function getICCVersion()
     return nil
 end
 
+local function linkMKL()
+    local libPrefix = ""
+    local libSuffix = "lib"
+    
+    if os.ishost("linux") then
+        libPrefix = "lib"
+        libSuffix = "a"
+    end
+    
+    local mkl64 = path.join(icpp, "mkl/lib/intel64/") .. libPrefix
+    filter "architecture:not x86"
+        if zpm.setting("blas95") then
+            links( mkl64 .. "mkl_blas95_lp64." .. libSuffix )
+        end
+        if zpm.setting("core") then
+            links( mkl64 .. "mkl_core." .. libSuffix )
+        end
+        if zpm.setting("intel") then
+            links( mkl64 .. "mkl_intel_lp64." .. libSuffix )
+        end
+        if zpm.setting("lapack95") then
+            links( mkl64 .. "mkl_lapack95_lp64." .. libSuffix )
+        end
+        if zpm.setting("sequential") then
+            links( mkl64 .. "mkl_sequential." .. libSuffix )
+        end
+        if zpm.setting("tbb") then
+            links( mkl64 .. "mkl_tbb_thread." .. libSuffix )
+        end
+
+    local mkl32 = path.join(icpp, "mkl/lib/ia32/") .. libPrefix
+    filter "architecture:x86"
+        if zpm.setting("blas95") then
+            links( mkl32 .. "mkl_blas95." .. libSuffix )
+        end
+        if zpm.setting("core") then
+            links( mkl32 .. "mkl_core." .. libSuffix )
+        end
+        if zpm.setting("intel") then
+            links( mkl32 .. "mkl_intel_c." .. libSuffix )
+        end
+        if zpm.setting("lapack95") then
+            links( mkl32 .. "mkl_lapack95." .. libSuffix )
+        end
+        if zpm.setting("sequential") then
+            links( mkl32 .. "mkl_sequential." .. libSuffix )
+        end
+        if zpm.setting("tbb") then
+            links( mkl32 .. "mkl_tbb_thread." .. libSuffix )
+        end
+end
+
 project "MKL"
     kind "StaticLib"
 
@@ -22,56 +74,15 @@ project "MKL"
     if icpp then
         zpm.export(function()
             includedirs(path.join(icpp, "mkl/include/"))    
+
+            if os.ishost("linux") then
+                linkMKL()
+            end
         end)
 
-        local libPrefix = ""
-        local libSuffix = "lib"
-        if os.ishost("linux") then
-            libPrefix = "lib"
-            libSuffix = "a"
+        if not os.ishost("linux") then
+            linkMKL()
         end
-        
-        local mkl64 = path.join(icpp, "mkl/lib/intel64/") .. libPrefix
-        filter "architecture:not x86"
-            if zpm.setting("blas95") then
-                links( mkl64 .. "mkl_blas95_lp64." .. libSuffix )
-            end
-            if zpm.setting("core") then
-                links( mkl64 .. "mkl_core." .. libSuffix )
-            end
-            if zpm.setting("intel") then
-                links( mkl64 .. "mkl_intel_lp64." .. libSuffix )
-            end
-            if zpm.setting("lapack95") then
-                links( mkl64 .. "mkl_lapack95_lp64." .. libSuffix )
-            end
-            if zpm.setting("sequential") then
-                links( mkl64 .. "mkl_sequential." .. libSuffix )
-            end
-            if zpm.setting("tbb") then
-                links( mkl64 .. "mkl_tbb_thread." .. libSuffix )
-            end
-
-        local mkl32 = path.join(icpp, "mkl/lib/ia32/") .. libPrefix
-        filter "architecture:x86"
-            if zpm.setting("blas95") then
-                links( mkl32 .. "mkl_blas95." .. libSuffix )
-            end
-            if zpm.setting("core") then
-                links( mkl32 .. "mkl_core." .. libSuffix )
-            end
-            if zpm.setting("intel") then
-                links( mkl32 .. "mkl_intel_c." .. libSuffix )
-            end
-            if zpm.setting("lapack95") then
-                links( mkl32 .. "mkl_lapack95." .. libSuffix )
-            end
-            if zpm.setting("sequential") then
-                links( mkl32 .. "mkl_sequential." .. libSuffix )
-            end
-            if zpm.setting("tbb") then
-                links( mkl32 .. "mkl_tbb_thread." .. libSuffix )
-            end
 
         filter {}
     else

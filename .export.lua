64eaf256d9787976b552754df0ec637b616ec609
m.vanduijn <m.vanduijn@zefiros.eu>
2018-03-30 02:43:46 +0200

    Revert changes

--- .export.lua
+++ .export.lua
@@ -1,6 +1,5 @@
 function getICCVersion()
     versions = {
-        "MKLROOT",
         "ICPP_COMPILER18", 
         "ICPP_COMPILER17", 
         "ICPP_COMPILER16", 
@@ -8,85 +7,64 @@ function getICCVersion()
     }
     for _, version in pairs(versions) do
         if os.getenv(version) ~= "" then
-            if version == "MKLROOT" then
-                return os.getenv(version), ""
-            else
-                return os.getenv(version), "mkl"
-            end
+            return os.getenv(version)
         end
     end
 
-    return nil, nil
-end
-
-local function linkMKL(icpp, mklDir)
-    local libPrefix = "/"
-    local libSuffix = ".lib"
-
-    if os.ishost("linux") then
-        libPrefix = "/lib"
-        libSuffix = ".a"
-    end
-    
-    local mkl64 = path.join(icpp, mklDir, "lib/intel64/") .. libPrefix
-    filter "architecture:not x86"
-        if zpm.setting("blas95") then
-            links( mkl64 .. "mkl_blas95_lp64" .. libSuffix )
-        end
-        if zpm.setting("core") then
-            links( mkl64 .. "mkl_core" .. libSuffix )
-        end
-        if zpm.setting("intel") then
-            links( mkl64 .. "mkl_intel_lp64" .. libSuffix )
-        end
-        if zpm.setting("lapack95") then
-            links( mkl64 .. "mkl_lapack95_lp64" .. libSuffix )
-        end
-        if zpm.setting("sequential") then
-            links( mkl64 .. "mkl_sequential" .. libSuffix )
-        end
-        if zpm.setting("tbb") then
-            links( mkl64 .. "mkl_tbb_thread" .. libSuffix )
-        end
-
-    local mkl32 = path.join(icpp, mklDir, "lib/ia32/") .. libPrefix
-    filter "architecture:x86"
-        if zpm.setting("blas95") then
-            links( mkl32 .. "mkl_blas95" .. libSuffix )
-        end
-        if zpm.setting("core") then
-            links( mkl32 .. "mkl_core" .. libSuffix )
-        end
-        if zpm.setting("intel") then
-            links( mkl32 .. "mkl_intel_c" .. libSuffix )
-        end
-        if zpm.setting("lapack95") then
-            links( mkl32 .. "mkl_lapack95" .. libSuffix )
-        end
-        if zpm.setting("sequential") then
-            links( mkl32 .. "mkl_sequential" .. libSuffix )
-        end
-        if zpm.setting("tbb") then
-            links( mkl32 .. "mkl_tbb_thread" .. libSuffix )
-        end
+    return nil
 end
 
 project "MKL"
     kind "StaticLib"
 
-    local icpp, mklDir = getICCVersion()
+    local icpp = getICCVersion()
+
     if icpp then
         zpm.export(function()
             includedirs(path.join(icpp, "mkl/include/"))    
-
-            if os.ishost("linux") then
-                linkMKL(icpp, mklDir)
-            end
         end)
+    
+        local mkl64 = path.join(icpp, "mkl/lib/intel64/")
+        filter "architecture:not x86"
+            if zpm.setting("blas95") then
+                links( mkl64 .. "/mkl_blas95_lp64.lib" )
+            end
+            if zpm.setting("core") then
+                links( mkl64 .. "/mkl_core.lib" )
+            end
+            if zpm.setting("intel") then
+                links( mkl64 .. "/mkl_intel_lp64.lib" )
+            end
+            if zpm.setting("lapack95") then
+                links( mkl64 .. "/mkl_lapack95_lp64.lib" )
+            end
+            if zpm.setting("sequential") then
+                links( mkl64 .. "/mkl_sequential.lib" )
+            end
+            if zpm.setting("tbb") then
+                links( mkl64 .. "/mkl_tbb_thread.lib" )
+            end
 
-        if not os.ishost("linux") then
-            linkMKL(icpp, mklDir)
-        end
+        local mkl32 = path.join(icpp, "mkl/lib/ia32/")
+        filter "architecture:x86"
+            if zpm.setting("blas95") then
+                links( mkl32 .. "/mkl_blas95.lib" )
+            end
+            if zpm.setting("core") then
+                links( mkl32 .. "/mkl_core.lib" )
+            end
+            if zpm.setting("intel") then
+                links( mkl32 .. "/mkl_intel_c.lib" )
+            end
+            if zpm.setting("lapack95") then
+                links( mkl32 .. "/mkl_lapack95.lib" )
+            end
+            if zpm.setting("sequential") then
+                links( mkl32 .. "/mkl_sequential.lib" )
+            end
+            if zpm.setting("tbb") then
+                links( mkl32 .. "/mkl_tbb_thread.lib" )
+            end
 
         filter {}
     else
